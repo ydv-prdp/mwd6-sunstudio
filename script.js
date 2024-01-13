ed95389b8734636707ebe95a178f96c2e2bef27b
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-01-13 12:51:11 +0530

    added page 3

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+
+var elemcontainer = document.querySelector("#elemcontainer");
+var fixedImageContainer = document.querySelector("#fixed-image");
+
+elemcontainer.addEventListener("mouseenter",function(){
+    fixedImageContainer.style.display = "block"
+})
+elemcontainer.addEventListener("mouseleave",function(){
+    fixedImageContainer.style.display = "none"
+})
+
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        var image = elem.getAttribute("data-image");
+        fixedImageContainer.style.backgroundImage = `url(${image})`
+    })
+})
